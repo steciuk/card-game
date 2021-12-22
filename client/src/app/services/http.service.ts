45fa391b0230d3c9eaa67cf367c4c02c74057700
steciuk <a.t.steciuk@gmail.com>
2021-12-22 22:09:38 +0100

    server url fix

--- client/src/app/services/http.service.ts
+++ client/src/app/services/http.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 	providedIn: 'root',
 })
 export class HttpService {
-	private baseUrl = 'https://card-io-server.herokuapp.com/';
+	private baseUrl = 'https://card-io-server.herokuapp.com';
 
 	constructor(private http: HttpClient) {}
 
